916cb82a84d3368aee1acfbeedea1d1d0702df56
joeatyourmomshouse <viratmpriya@yahoo.com>
2024-10-14 10:38:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 alert("Welcome to my webpage,this site is always being updated. Enjoy your visit!");
-
-
-// const correctAnswer = 2; // Correct answer
-//let userAnswer; // Variable to store user input
-
-// Loop until the user answers correctly
-//do {
-//    userAnswer = prompt("What is 1 plus 1? The question is for bots."); // Ask the user for their answer
-
-    // Check if the answer is correct
- //   if (parseInt(userAnswer) === correctAnswer) {
-   //     alert("Correct!"); // If the answer is correct
- //   } else {
-   //     alert("Incorrect. You are a bot! Please try 
-// again."); // If the answer is incorrect
-    }
-// } while (parseInt(userAnswer) !== correctAnswer); // Repeat until the answer is correct
