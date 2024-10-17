250ed6e8e3dd16b9923458f8992e5f2bdd04373a
joeatyourmomshouse <viratmpriya@yahoo.com>
2024-10-17 13:53:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-alert("Welcome to my webpage,this site is always being updated. Enjoy your visit!");
+alert("Welcome to my webpage,email 800002456@vistausd.org or viratmpriya@yahoo.com to ask for an link if your current one does not work.");
