b992dd66ccf620317af93822fb8f6d3773265e24
joeatyourmomshouse <viratmpriya@yahoo.com>
2024-10-18 14:40:05 +0000

    w

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 alert("Welcome to my webpage,email 800002456@vistausd.org or viratmpriya@yahoo.com to ask for an link if your current one does not work.");
+
+const sikeSound = new Audio("My Project.mp3");
+
+function play_Sike() {
+    sikeSound.play();
+}
+
